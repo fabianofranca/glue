235838eaed436a2aad857dbad0771133724af25b
Fabiano França <email@fabianofranca.com>
2018-05-07 00:15:42 +0000

    Create project and basic test

---
+++ src/androidTest/kotlin/glue/GlueTest.kt
@@ -0,0 +1,16 @@
+package glue
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class GlueTest {
+   val context = InstrumentationRegistry.getContext()
+
+    @Test
+    fun shouldWorks() {
+
+    }
+}
